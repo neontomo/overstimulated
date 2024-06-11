109457ba972329eefd7845c5cb4514ec45df5a5c
Tomo <105588693+neontomo@users.noreply.github.com>
2024-06-11 21:25:16 +0200

    Initial Commit

---
+++ overstimulated/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  overstimulated
+//
+//  Created by Tomo Myrman on 2024-06-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
